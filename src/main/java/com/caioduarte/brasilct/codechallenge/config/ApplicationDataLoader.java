0a5255e123d9ce624d729fd43f89747e6e1ba541
Caio Duarte <ca.andrade.duarte@gmail.com>
2014-07-07 22:13:21 -0300

    Adicionado carregamento dos dados de rotas e linhas ao ApplicationDataLoader

--- src/main/java/com/caioduarte/brasilct/codechallenge/config/ApplicationDataLoader.java
+++ src/main/java/com/caioduarte/brasilct/codechallenge/config/ApplicationDataLoader.java
@@ -7,8 +7,14 @@ import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import com.caioduarte.brasilct.codechallenge.dao.LineDao;
+import com.caioduarte.brasilct.codechallenge.dao.RouteDao;
 import com.caioduarte.brasilct.codechallenge.dao.StationDao;
+import com.caioduarte.brasilct.codechallenge.dataloaders.LineLoader;
+import com.caioduarte.brasilct.codechallenge.dataloaders.RouteLoader;
 import com.caioduarte.brasilct.codechallenge.dataloaders.StationLoader;
+import com.caioduarte.brasilct.codechallenge.models.Line;
+import com.caioduarte.brasilct.codechallenge.models.Route;
 import com.caioduarte.brasilct.codechallenge.models.Station;
 
 @Component
@@ -21,18 +27,50 @@ public class ApplicationDataLoader implements InitializingBean {
 	
 	@Autowired
 	private StationDao stationDao;
+	
+	@Autowired
+	private RouteLoader routeLoader;
+	
+	@Autowired
+	private RouteDao routeDao;
+	
+	@Autowired
+	private LineLoader lineLoader;
+	
+	@Autowired
+	private LineDao lineDao;
 
 	@Override
 	public void afterPropertiesSet() throws Exception {
 		loadStationData();
+		
+		loadRouteData();
+		
+		loadLineData();
 	}
-
+	
 	protected void loadStationData() {
 		List<Station> stations = stationLoader.load();
 		
 		stationDao.saveAll(stations);
 		
 		logger.info("Stations load successfully complete");
+	}
+
+	protected void loadRouteData() {
+		List<Route> routes = routeLoader.load();
+		
+		routeDao.saveAll(routes);
+		
+		logger.info("Routes load successfully complete");
+	}
+	
+	protected void loadLineData() {
+		List<Line> lines = lineLoader.load();
+		
+		lineDao.saveAll(lines);
+		
+		logger.info("Lines load successfully complete");
 	}
 	
 }
--- src/test/java/com/caioduarte/brasilct/codechallenge/config/ApplicationDataLoaderTest.java
+++ src/test/java/com/caioduarte/brasilct/codechallenge/config/ApplicationDataLoaderTest.java
@@ -12,8 +12,14 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
+import com.caioduarte.brasilct.codechallenge.dao.LineDao;
+import com.caioduarte.brasilct.codechallenge.dao.RouteDao;
 import com.caioduarte.brasilct.codechallenge.dao.StationDao;
+import com.caioduarte.brasilct.codechallenge.dataloaders.LineLoader;
+import com.caioduarte.brasilct.codechallenge.dataloaders.RouteLoader;
 import com.caioduarte.brasilct.codechallenge.dataloaders.StationLoader;
+import com.caioduarte.brasilct.codechallenge.models.Line;
+import com.caioduarte.brasilct.codechallenge.models.Route;
 import com.caioduarte.brasilct.codechallenge.models.Station;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -24,6 +30,18 @@ public class ApplicationDataLoaderTest {
 	
 	@Mock
 	private StationDao stationDao;
+
+	@Mock
+	private RouteLoader routeLoader;
+	
+	@Mock
+	private RouteDao routeDao;
+	
+	@Mock
+	private LineLoader lineLoader;
+	
+	@Mock
+	private LineDao lineDao;
 	
 	@InjectMocks
 	private ApplicationDataLoader loader = new ApplicationDataLoader();
@@ -38,6 +56,30 @@ public class ApplicationDataLoaderTest {
 		
 		verify(stationLoader).load();
 		verify(stationDao).saveAll(eq(stationsLoaded));
+	}
+	
+	@Test
+	public void must_load_the_routes_and_persist_them() {
+		ArrayList<Route> routesLoaded = new ArrayList<Route>();
+		
+		when(routeLoader.load()).thenReturn(routesLoaded);
+		
+		loader.loadRouteData();
+		
+		verify(routeLoader).load();
+		verify(routeDao).saveAll(eq(routesLoaded));
+	}
+	
+	@Test
+	public void must_load_the_lines_and_persist_them() {
+		ArrayList<Line> linesLoaded = new ArrayList<Line>();
+		
+		when(lineLoader.load()).thenReturn(linesLoaded);
+		
+		loader.loadLineData();
+		
+		verify(lineLoader).load();
+		verify(lineDao).saveAll(eq(linesLoaded));
 	}
 	
 }
